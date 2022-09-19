bdc18391e7ea27cd3c4c0349c4dd914f8e06f354
mom <stu6412732107@sskru.ac.th>
2022-09-19 14:23:25 +0700

    asdad

--- train/settings.py
+++ train/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'web',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'train.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . imoort views
+from . import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),
-    path('about/', views.About, name='about')
-    path('contact/', views.Contact,name='contact')
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
 ]
 
 
